04147bc97ac2f95e65f49b82b82f785078907042
LinhHN13 <linhhn13@fe.edu.vn>
2025-06-13 23:34:50 +0700

    update env key

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,5 +5,8 @@ class Settings:
     DB_USER: str = os.getenv("DB_USER", "testuser")
     DB_PASSWORD: str = os.getenv("DB_PASSWORD", "testpass")
     DB_NAME: str = os.getenv("DB_NAME", "testdb")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "your_secret_key")
+    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 settings = Settings()
