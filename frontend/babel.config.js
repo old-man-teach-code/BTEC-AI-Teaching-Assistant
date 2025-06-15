c8e5c44587f714b2f0ae8649d8b6527733cef37b
LinhHN13 <linhhn13@fe.edu.vn>
2025-06-15 10:01:58 +0700

    update frontend

--- frontend/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: [
-    ['@vue/cli-plugin-babel/preset', { requireConfigFile: true }],
-    ['@babel/preset-env', { requireConfigFile: true }],
-  ],
-  babelrc: false,
-  configFile: false
-};
